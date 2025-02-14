fc878b961e43f23c205a923dd44ec3e5b8ceb32f
NIVELZ69 <145551712+NIVELZ69@users.noreply.github.com>
2025-02-14 16:20:46 +0000

    Comentarios JavaDoc TurnoListener

--- src/main/java/dad/mundoria/model/TurnoListener.java
+++ src/main/java/dad/mundoria/model/TurnoListener.java
@@ -1,7 +1,23 @@
 package dad.mundoria.model;
 
+/**
+ * Interfaz que define los métodos necesarios para manejar los turnos
+ * de los jugadores y los enemigos durante el combate.
+ * Las clases que implementen esta interfaz serán responsables de
+ * gestionar los eventos de los turnos, notificando cuando le corresponde
+ * al jugador o al enemigo realizar una acción.
+ */
 public interface TurnoListener {
-	void turnoJugador(Personaje personaje);
 
-	void turnoEnemigo();
+    /**
+     * Método llamado cuando es el turno de un jugador para realizar una acción.
+     * 
+     * @param personaje El personaje cuyo turno está en curso.
+     */
+    void turnoJugador(Personaje personaje);
+
+    /**
+     * Método llamado cuando es el turno de un enemigo para realizar una acción.
+     */
+    void turnoEnemigo();
 }
